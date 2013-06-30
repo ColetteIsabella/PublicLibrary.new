97de85ddc8de2f10f29e8589e405ef8a756d5b3e
Colette Taylor <coletteisabella@gmail.com>
2013-06-30 13:04:50 -0700

    First commit with untested material
    I have been battling with hardware issues to get a proper Ruby
    environment set up on my computer, this is a bit hacky as I have had to
    use terminal to check my work, and it is a bit spotty.

---
+++ tryagain.rb
@@ -0,0 +1,42 @@
+class Library
+def shelves
+	ary = [1, 2, 3, 4]
+end	
+		books = ["A Wrinkle in Time", "Blindsight", "Contact", "Dirk Gently"]
+		books.each do|collection|
+			puts "These are the books you can check out:" + collection 
+	end
+end
+
+class Shelf
+		hsh = contents={1 => "A Wrinkle in Time", 
+						2 => "Blindsight" 
+						3 => "Contact",
+						4 => "Dirk Gently"}
+
+	hsh.each do |key, value|
+		return key, " contains ", value
+	end
+end
+
+class Book
+	def initialize (title, stack)
+			@title=title
+			@stack=stack
+	end
+	def enshelf()		
+			contents_1=contents.new("A Wrinkle in Time", 1)
+			contents_2=contents.new("Blindsight", 2)
+			contents_3=contents.new("Contact", 3)
+			contents_4=contents.new("Dirk Gently", 4)
+			return "#{title} is returned to #{stack}"
+		def unshelf()	
+		title=gets.chomp
+		if books[title.to_sym].nil
+			return "#{title} does not exist in this library"
+		else
+			books.delete(title.to_sym)
+			return "#{title}has been checked out"
+		end
+
+		
